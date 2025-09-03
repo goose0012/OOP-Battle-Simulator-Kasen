cbb69a8df6ffcbb97715ea1af67d6181773a8686
Kasen Davis <dkasen751@gmail.com>
2025-09-03 09:49:54 -0500

    Implement Hero class attributes and methods
    Bro can attack and die

--- hero.py
+++ hero.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Hero:
     """
     This is our hero blueprint.
@@ -13,6 +16,11 @@ class Hero:
     """
     
     def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.defense = random.randint(5, 15)
+        self.attack_power = random.randint(10, 20)
+        self.ultimate_ability = "Thank you king!"
         #TODO Set the hero's name.
         #TODO Set the hero's health. You might give the hero more health than a goblin.
         #TODO Set the hero's attack power. Should it be more consistent than the goblin's?
@@ -20,9 +28,16 @@ class Hero:
 
     def strike(self):
         # TODO Implement the hero's attack logic. It could be stronger or more consistent than a goblin's.
-    
+        return random.randint(1, self.attack_power)
+
     def receive_damage(self, damage):
         # TODO Implement take_damage
         # TODO We should prevent health from going into the NEGATIVE
-    
-    #TODO define is_alive
+        net_damage = damage - self.defense
+        if net_damage > 0:
+            self.health -= net_damage
+        if self.health < 0:
+            self.health = 0
+
+    def is_alive(self):
+        return self.health > 0
